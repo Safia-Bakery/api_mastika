e5c5e4c337961550d59c12a09e10c0e8bc421204
Gayratbek <gayratbekakhmedov@gmail.com>
2023-10-19 19:26:48 +0500

    first comit

--- users/routers/user_router.py
+++ users/routers/user_router.py
@@ -54,7 +54,7 @@ async def get_user(is_client:Optional[int]=None,id:Optional[int]=None,db:Session
 
 
 
-@user_router.get('/me',tags=['Users'],response_model=User)
+@user_router.get('/me',tags=['Users'],response_model=UserMe)
 async def get_user(db:Session=Depends(get_db),request_user:User=Depends(get_current_user)):
     if request_user.user_role is not None:
         data = {i.pagecrud_id:True for i in request_user.user_role.role_permission}
