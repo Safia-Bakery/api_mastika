bc0dff4e63d62653184a7cb6ccb8be51b792aec8
Gayratbek <gayratbekakhmedov@gmail.com>
2023-12-14 12:22:23 +0500

    edit color

--- apis/utils/api_micro.py
+++ apis/utils/api_micro.py
@@ -65,7 +65,7 @@ def sendtotelegram(bot_token,chat_id,message_text,files):
     }
 
     # Send the request to send the inline keyboard message
-    response = requests.post(f'https://api.telegram.org/bot{bot_token}/sendMessage', json=payload,files=files)
+    response = requests.post(f'https://api.telegram.org/bot{bot_token}/sendPhoto', json=payload,files=files)
     print(response.content)
     # Check the response status
     if response.status_code == 200:
